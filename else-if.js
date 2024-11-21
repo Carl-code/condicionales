4c8c6d381b7377f748c4209617086f21d2dea40f
Carl-code <arimatea865@gmail.com>
2024-11-21 09:57:16 -0600

    first commit

---
+++ else-if.js
@@ -0,0 +1,9 @@
+const fruta = "manzana";
+
+if (fruta === "manzana") {
+  console.log("La fruta es una manzana");
+} else if (fruta === "pera") {
+  console.log("La fruta es una pera");
+} else {
+  console.log("La fruta es otra cosa");
+}
---
+++ if.js
@@ -0,0 +1,9 @@
+//cambia esta variable para ver como se evalua la condicion
+const num = 15;
+const condicion = num > 3;
+
+if (condicion) {
+  console.log("La condición es verdadera");
+}
+
+console.log("esto se ejecuta siempre porque está fuera del if");
